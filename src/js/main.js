00c8f7076d43d9fdcd6725ad896ff719145c62f7
Carlos Barranco <caba9313@gmail.com>
2016-12-12 02:05:19 -0600

    Change from grid to layout

--- src/js/main.js
+++ src/js/main.js
@@ -1,38 +1,39 @@
-var aligmnent = 'top-xs';
-var distribution = 'start-xs';
+var horizontal = '-stretch';
+var vertical = '-start';
 var direction = 'row';
 var sandbox;
 
 setDirection = function(direc) {
 	switch(direc){
 	case 'row':
-		sandbox.classList.remove('col', 'reverse');
-		sandbox.classList.add('row');
+		sandbox.classList.remove('layout-column', 'layout-column-reverse', 'layout-row-reverse');
+		sandbox.classList.add('layout-row');
 		break;
 	case 'col':
-		sandbox.classList.remove('reverse');
-		sandbox.classList.add('col');
+		sandbox.classList.remove('layout-column-reverse', 'layout-row', 'layout-row-reverse');
+		sandbox.classList.add('layout-column');
 		break;
 	case 'row reverse':
-		sandbox.classList.remove('col');
-		sandbox.classList.add('reverse');
+		sandbox.classList.remove('layout-column-reverse', 'layout-column', 'layout-row-reverse');
+		sandbox.classList.add('layout-row-reverse');
 		break;
 	case 'col reverse':
-		sandbox.classList.add('col', 'reverse');
+		sandbox.classList.remove('layout-column', 'layout-row', 'layout-row-reverse');
+		sandbox.classList.add('layout-column-reverse');
 		break;
 	}
 };
 
-setAligment = function(align) {
-	sandbox.classList.remove(aligmnent);
-	aligmnent = align + '-xs';
-	sandbox.classList.add(aligmnent);
+setHorizontal = function(horiz) {
+	sandbox.classList.remove(`layout-align${vertical}${horizontal}`);
+	horizontal = horiz;
+	sandbox.classList.add(`layout-align${vertical}${horizontal}`);
 };
 
-setDistribution = function(distrib) {
-	sandbox.classList.remove(distribution);
-	distribution = distrib + '-xs';
-	sandbox.classList.add(distribution);
+setVertical = function(vert) {
+	sandbox.classList.remove(`layout-align${vertical}${horizontal}`);
+	vertical = vert;
+	sandbox.classList.add(`layout-align${vertical}${horizontal}`);
 };
 
 setTimeout(()=>{
