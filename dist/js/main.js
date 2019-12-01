2e75528d26af70efeb5d21f7ed8cf1ec2f2a43c6
cabaag <caba9313@gmail.com>
2019-12-01 13:00:16 -0600

    Updating all

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,41 +1,43 @@
-var horizontal = '-stretch';
-var vertical = '-start';
-var direction = 'row';
-var sandbox;
+let horizontal = '-stretch';
+let vertical = '-start';
+const direction = 'row';
+let sandbox;
 
-setDirection = function(direc) {
-	switch(direc){
-	case 'row':
-		sandbox.classList.remove('layout-column', 'layout-column-reverse', 'layout-row-reverse');
-		sandbox.classList.add('layout-row');
-		break;
-	case 'col':
-		sandbox.classList.remove('layout-column-reverse', 'layout-row', 'layout-row-reverse');
-		sandbox.classList.add('layout-column');
-		break;
-	case 'row reverse':
-		sandbox.classList.remove('layout-column-reverse', 'layout-column', 'layout-row-reverse');
-		sandbox.classList.add('layout-row-reverse');
-		break;
-	case 'col reverse':
-		sandbox.classList.remove('layout-column', 'layout-row', 'layout-row-reverse');
-		sandbox.classList.add('layout-column-reverse');
-		break;
-	}
-};
+function setDirection(direc) {
+   switch (direc) {
+      case 'row':
+         sandbox.classList.remove('column', 'column-reverse', 'row-reverse');
+         sandbox.classList.add('row');
+         break;
+      case 'col':
+         sandbox.classList.remove('column-reverse', 'row', 'row-reverse');
+         sandbox.classList.add('column');
+         break;
+      case 'row reverse':
+         sandbox.classList.remove('column-reverse', 'column', 'row-reverse');
+         sandbox.classList.add('row-reverse');
+         break;
+      case 'col reverse':
+         sandbox.classList.remove('column', 'row', 'row-reverse');
+         sandbox.classList.add('column-reverse');
+         break;
+      default:
+         break;
+   }
+}
 
-setHorizontal = function(horiz) {
-	sandbox.classList.remove(`layout-align${vertical}${horizontal}`);
-	horizontal = horiz;
-	sandbox.classList.add(`layout-align${vertical}${horizontal}`);
-};
+function setHorizontal(horiz) {
+   sandbox.classList.remove(`align${vertical}${horizontal}`);
+   horizontal = horiz;
+   sandbox.classList.add(`align${vertical}${horizontal}`);
+}
 
-setVertical = function(vert) {
-	sandbox.classList.remove(`layout-align${vertical}${horizontal}`);
-	vertical = vert;
-	sandbox.classList.add(`layout-align${vertical}${horizontal}`);
-};
+function setVertical(vert) {
+   sandbox.classList.remove(`align${vertical}${horizontal}`);
+   vertical = vert;
+   sandbox.classList.add(`align${vertical}${horizontal}`);
+}
 
-setTimeout(()=>{
-	sandbox = document.querySelector('.sandbox-container'); 
+setTimeout(() => {
+   sandbox = document.querySelector('.sandbox-container');
 });
--- gulpfile.js
+++ gulpfile.js
@@ -6,56 +6,64 @@ const rename = require('gulp-rename');
 const connect = require('gulp-connect');
 
 const pug = require('gulp-pug');
-
 const postcss = require('gulp-postcss');
 const sass = require('gulp-sass');
+const cssnano = require('cssnano');
 
-const getTime = function () {
-	let now = new Date();
-	let hour = now.getHours();
-	let minutes = now.getMinutes() > 9 ? now.getMinutes() : `0${now.getMinutes()}`;
-	let seconds = now.getSeconds() > 9 ? now.getSeconds() : `0${now.getSeconds()}`;
-	return `${hour}:${minutes}:${seconds}`;
-};
+const autoprefixer = require('autoprefixer');
 
-gulp.task('connect', function () {
-	connect.server({
-		root: '',
-		livereload: true
-	});
-});
+function getTime() {
+   const now = new Date();
+   const hour = now.getHours();
+   const minutes = now.getMinutes() > 9 ? now.getMinutes() : `0${now.getMinutes()}`;
+   const seconds = now.getSeconds() > 9 ? now.getSeconds() : `0${now.getSeconds()}`;
+   return `${hour}:${minutes}:${seconds}`;
+}
 
-gulp.task('pug', function () {
-	console.log('[' + chalk.grey(getTime()) + '] Compiling \'' + chalk.cyan('pug') + '\'');
-	return gulp.src('./src/index.pug')
-		.pipe(pug().on('error', gutil.log))
-		.pipe(gulp.dest('./'))
-		.pipe(connect.reload());
-});
+function server(done) {
+   connect.server({
+      root: '',
+      livereload: true,
+   });
+   done();
+}
 
-gulp.task('postcss', function () {
-	console.log('[' + chalk.grey(getTime()) + '] Compiling \'' + chalk.cyan('postcss') + '\'');
-	return gulp.src('./src/sass/*.scss')
-		.pipe(sass().on('error', sass.logError))
-		.pipe(postcss([require('autoprefixer')]))
-		.pipe(gulp.dest('./dist/css/'))
-		.pipe(postcss([require('cssnano')]))
-		.pipe(rename({extname: '.min.css'}))
-		.pipe(gulp.dest('./dist/css/'))
-		.pipe(connect.reload());
-});
+function Pug() {
+   console.log(`[${chalk.grey(getTime())}] Compiling '${chalk.cyan('Pug')}'`);
+   return gulp
+      .src('./src/index.pug')
+      .pipe(pug().on('error', gutil.log))
+      .pipe(gulp.dest('./'))
+      .pipe(connect.reload());
+}
 
-gulp.task('js', function () {
-	console.log('[' + chalk.grey(getTime()) + '] Compiling \'' + chalk.cyan('js') + '\'');
-	return gulp.src('./src/js/*.js')
-		.pipe(gulp.dest('./dist/js/'))
-		.pipe(connect.reload());
-});
+function PostCSS() {
+   console.log(`[${chalk.grey(getTime())}] Compiling '${chalk.cyan('PostCSS')}'`);
+   return gulp
+      .src('./src/sass/*.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(postcss([autoprefixer]))
+      .pipe(gulp.dest('./dist/css/'))
+      .pipe(postcss([cssnano]))
+      .pipe(rename({ extname: '.min.css' }))
+      .pipe(gulp.dest('./dist/css/'))
+      .pipe(connect.reload());
+}
 
-gulp.task('watch', function () {
-	gulp.watch(['./src/**/*.pug'], ['pug']);
-	gulp.watch(['./src/**/*.scss'], ['postcss']);
-	gulp.watch(['./src/**/*.js'], ['js']);
-});
+function JavaScript() {
+   console.log(`[${chalk.grey(getTime())}] Compiling '${chalk.cyan('JavaScript')}'`);
+   return gulp
+      .src('./src/js/*.js')
+      .pipe(gulp.dest('./dist/js/'))
+      .pipe(connect.reload());
+}
 
-gulp.task('default', ['connect', 'pug', 'postcss', 'js', 'watch']);
+function watch() {
+   gulp.watch('./src/**/*.pug').on('all', gulp.series(Pug));
+   gulp.watch('./src/**/*.scss').on('all', gulp.series(PostCSS));
+   gulp.watch('./src/**/*.js').on('all', gulp.series(JavaScript));
+}
+
+gulp.task('build', gulp.parallel(Pug, PostCSS, JavaScript));
+
+gulp.task('default', gulp.series('build', server, watch));
