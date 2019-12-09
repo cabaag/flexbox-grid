683209410a61ed1ec0c197ff216a41d0957c4c44
cabaag <caba9313@gmail.com>
2019-12-08 21:08:58 -0600

    Improvemenets

--- dist/js/main.js
+++ dist/js/main.js
@@ -8,75 +8,75 @@ let items = 3;
 let sandbox;
 
 function setDirection(direc) {
-   switch (direc) {
-      case 'row':
-         sandbox.classList.remove('column', 'column-reverse', 'row-reverse');
-         sandbox.classList.add('row');
-         break;
-      case 'col':
-         sandbox.classList.remove('column-reverse', 'row', 'row-reverse');
-         sandbox.classList.add('column');
-         break;
-      case 'row reverse':
-         sandbox.classList.remove('column-reverse', 'column', 'row-reverse');
-         sandbox.classList.add('row-reverse');
-         break;
-      case 'col reverse':
-         sandbox.classList.remove('column', 'row', 'row-reverse');
-         sandbox.classList.add('column-reverse');
-         break;
-      default:
-         break;
-   }
+  switch (direc) {
+    case 'row':
+      sandbox.classList.remove('column', 'column-reverse', 'row-reverse');
+      sandbox.classList.add('row');
+      break;
+    case 'col':
+      sandbox.classList.remove('column-reverse', 'row', 'row-reverse');
+      sandbox.classList.add('column');
+      break;
+    case 'row reverse':
+      sandbox.classList.remove('column-reverse', 'column', 'row-reverse');
+      sandbox.classList.add('row-reverse');
+      break;
+    case 'col reverse':
+      sandbox.classList.remove('column', 'row', 'row-reverse');
+      sandbox.classList.add('column-reverse');
+      break;
+    default:
+      break;
+  }
 }
 
 function changeChildren() {
-   if (document.getElementById('children').value < 0 && document.getElementById('children').value > 40) {
-      return;
-   }
-   items = +document.getElementById('children').value;
-   sandbox.innerHTML = '';
-   console.log(items);
-   for (let i = 1; i < items + 1; i += 1) {
-      console.log(i);
-      const node = document.createElement('div');
-      node.classList.add('box', 'ma-2');
-      node.style.padding = `${i * 2}px`;
-      sandbox.appendChild(node);
-   }
+  if (document.getElementById('children').value < 0 && document.getElementById('children').value > 40) {
+    return;
+  }
+  items = +document.getElementById('children').value;
+  sandbox.innerHTML = '';
+  console.log(items);
+  for (let i = 1; i < items + 1; i += 1) {
+    console.log(i);
+    const node = document.createElement('div');
+    node.classList.add('box', 'ma-2');
+    node.style.padding = `${i * 2}px`;
+    sandbox.appendChild(node);
+  }
 }
 
 function setWrap(newWrap) {
-   sandbox.classList.remove(wrap);
-   wrap = newWrap;
-   sandbox.classList.add(newWrap);
+  sandbox.classList.remove(wrap);
+  wrap = newWrap;
+  sandbox.classList.add(newWrap);
 }
 
 function setJustifyContent(newHorizontal) {
-   sandbox.classList.remove(justifyContent);
-   justifyContent = newHorizontal;
-   sandbox.classList.add(newHorizontal);
+  sandbox.classList.remove(justifyContent);
+  justifyContent = newHorizontal;
+  sandbox.classList.add(newHorizontal);
 }
 
 function setJustifyItems(newHorizontal) {
-   sandbox.classList.remove(justifyItems);
-   justifyItems = newHorizontal;
-   sandbox.classList.add(newHorizontal);
+  sandbox.classList.remove(justifyItems);
+  justifyItems = newHorizontal;
+  sandbox.classList.add(newHorizontal);
 }
 
 function setAlignContent(newVertical) {
-   sandbox.classList.remove(alignContent);
-   alignContent = newVertical;
-   sandbox.classList.add(newVertical);
+  sandbox.classList.remove(alignContent);
+  alignContent = newVertical;
+  sandbox.classList.add(newVertical);
 }
 
 function setAlignItems(newVertical) {
-   sandbox.classList.remove(alignItems);
-   alignItems = newVertical;
-   sandbox.classList.add(newVertical);
+  sandbox.classList.remove(alignItems);
+  alignItems = newVertical;
+  sandbox.classList.add(newVertical);
 }
 
 setTimeout(() => {
-   sandbox = document.querySelector('.sandbox-container');
-   changeChildren();
+  sandbox = document.querySelector('.sandbox-container');
+  changeChildren();
 });
