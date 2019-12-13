ef3f08f30f54adab0b3fda2a78fe5dc4f82b0790
cabaag <caba9313@gmail.com>
2019-12-12 20:20:00 -0600

    migrating to gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -1,61 +1,81 @@
-const gulp = require('gulp');
-const gutil = require('gulp-util');
-const chalk = require('chalk');
-const rename = require('gulp-rename');
+const gulp = require("gulp");
+const gutil = require("gulp-util");
+const chalk = require("chalk");
+const rename = require("gulp-rename");
 
-const connect = require('gulp-connect');
+const gulpConnect = require("gulp-connect");
 
-const pug = require('gulp-pug');
+const gulpPug = require("gulp-pug");
 
-const postcss = require('gulp-postcss');
-const sass = require('gulp-sass');
+const gulpPostcss = require("gulp-postcss");
+const gulpSass = require("gulp-sass");
 
-const getTime = function () {
+const getTime = function() {
 	let now = new Date();
 	let hour = now.getHours();
-	let minutes = now.getMinutes() > 9 ? now.getMinutes() : `0${now.getMinutes()}`;
-	let seconds = now.getSeconds() > 9 ? now.getSeconds() : `0${now.getSeconds()}`;
+	let minutes =
+		now.getMinutes() > 9 ? now.getMinutes() : `0${now.getMinutes()}`;
+	let seconds =
+		now.getSeconds() > 9 ? now.getSeconds() : `0${now.getSeconds()}`;
 	return `${hour}:${minutes}:${seconds}`;
 };
 
-gulp.task('connect', function () {
-	connect.server({
-		root: '',
+function connect(cb) {
+	gulpConnect.server({
+		root: "",
 		livereload: true
 	});
-});
+	cb();
+}
 
-gulp.task('pug', function () {
-	console.log('[' + chalk.grey(getTime()) + '] Compiling \'' + chalk.cyan('pug') + '\'');
-	return gulp.src('./src/index.pug')
-		.pipe(pug().on('error', gutil.log))
-		.pipe(gulp.dest('./'))
-		.pipe(connect.reload());
-});
+function pug() {
+	console.log(
+		"[" + chalk.grey(getTime()) + "] Compiling '" + chalk.cyan("pug") + "'"
+	);
+	return gulp
+		.src("./src/index.pug")
+		.pipe(gulpPug().on("error", gutil.log))
+		.pipe(gulp.dest("./"))
+		.pipe(gulpConnect.reload());
+}
 
-gulp.task('postcss', function () {
-	console.log('[' + chalk.grey(getTime()) + '] Compiling \'' + chalk.cyan('postcss') + '\'');
-	return gulp.src('./src/sass/*.scss')
-		.pipe(sass().on('error', sass.logError))
-		.pipe(postcss([require('autoprefixer')]))
-		.pipe(gulp.dest('./dist/css/'))
-		.pipe(postcss([require('cssnano')]))
-		.pipe(rename({extname: '.min.css'}))
-		.pipe(gulp.dest('./dist/css/'))
-		.pipe(connect.reload());
-});
+function postcss() {
+	console.log(
+		"[" + chalk.grey(getTime()) + "] Compiling '" + chalk.cyan("postcss") + "'"
+	);
+	return gulp
+		.src("./src/sass/*.scss")
+		.pipe(gulpSass().on("error", gulpSass.logError))
+		.pipe(gulpPostcss([require("autoprefixer")]))
+		.pipe(gulp.dest("./dist/css/"))
+		.pipe(gulpPostcss([require("cssnano")]))
+		.pipe(rename({ extname: ".min.css" }))
+		.pipe(gulp.dest("./dist/css/"))
+		.pipe(gulpConnect.reload());
+}
 
-gulp.task('js', function () {
-	console.log('[' + chalk.grey(getTime()) + '] Compiling \'' + chalk.cyan('js') + '\'');
-	return gulp.src('./src/js/*.js')
-		.pipe(gulp.dest('./dist/js/'))
-		.pipe(connect.reload());
-});
+function javascript() {
+	console.log(
+		"[" +
+			chalk.grey(getTime()) +
+			"] Compiling '" +
+			chalk.cyan("javascript") +
+			"'"
+	);
+	return gulp
+		.src("./src/js/*.js")
+		.pipe(gulp.dest("./dist/js/"))
+		.pipe(gulpConnect.reload());
+}
 
-gulp.task('watch', function () {
-	gulp.watch(['./src/**/*.pug'], ['pug']);
-	gulp.watch(['./src/**/*.scss'], ['postcss']);
-	gulp.watch(['./src/**/*.js'], ['js']);
-});
+function watch() {
+	gulp.watch("./src/**/*.pug", pug);
+	gulp.watch("./src/**/*.scss", postcss);
+	gulp.watch("./src/**/*.js", javascript);
+}
 
-gulp.task('default', ['connect', 'pug', 'postcss', 'js', 'watch']);
+const build = gulp.parallel(pug, postcss, javascript);
+const defaultTask = gulp.parallel(build, watch, connect);
+
+exports.build = build;
+exports.default = gulp.series(defaultTask, build);
